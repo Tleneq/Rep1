14bbccdfa60775c918df9b3ddab332bd0787086b
aleksmezgier <121099740+aleksmezgier@users.noreply.github.com>
2023-01-27 19:20:28 +0100

    Add files via upload
    Dodanie możliwości mnożenia.

---
+++ mnozenie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double a, b;
+  cout << "Wypisz dwie liczby do pomnożenia:" << endl;
+  cin >> a;
+  cin >> b;
+  cout << "Wynik mnożenia: " << a * b << endl;
+  return 0;
+}
