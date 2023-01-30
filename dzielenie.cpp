4185ee1a5d36d9b3e605c09ae0ed29429d3df588
aleksmezgier <121099740+aleksmezgier@users.noreply.github.com>
2023-01-30 12:58:26 +0100

    Update dzielenie.cpp

--- dzielenie.cpp
+++ dzielenie.cpp
@@ -3,3 +3,11 @@ using namespace std;
 long double dzielenie(long double a, long double b){
     return a/b;
 }
+int main(int argc, char* argv[]){
+    long double a,b;
+    cout<<"Wpisz dwie liczby do podzielenia: ";
+    cin>>a>>b;
+    if(!b) cout<<"Pamiętaj *******, nie dziel przez zero!";
+    else cout<<"Wynik: "<<dzielenie(a,b);
+    return 0;
+}
